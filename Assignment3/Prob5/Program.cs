66d466cc2049fb31599f88b54412482c8fe8a8ac
Bogdan Livadaru <bogdanlivadaru@yahoo.com>
2019-10-29 20:23:30 -0700

    All Done

---
+++ Assignment3/Prob5/Program.cs
@@ -0,0 +1,56 @@
+﻿/*
+Author: Bogdan Livadaru
+Date:   10/21/2019
+CTEC 135: Microsoft Software Development with C#
+​
+Module 4: Programming Assignment 3, Problem 5
+  * Create a class with two fields, one a string and the other an int. 
+      The class implements the IComparable interface.
+  * Create appropriate methods for the class
+  * Objects in the class should be ordered in the following manner:
+      - primary sort order is alphabetical according to the text field
+      - secondary sort order is largest first according to the int field
+  * Demonstrate the the objects can be sorted according to requirement
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prob5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create some objects to use for testing
+            SimpleClass sc1 = new SimpleClass("B", 2);
+            SimpleClass sc2 = new SimpleClass("A", 1);
+            SimpleClass sc3 = new SimpleClass("C", 1);
+            SimpleClass sc4 = new SimpleClass("B", 1);
+            SimpleClass sc5 = new SimpleClass("B", 2);
+            SimpleClass sc6 = new SimpleClass("B", 3);
+            
+            // test logic - expect positive, i.e. B follows A
+            Console.WriteLine(sc1.CompareTo(sc2));
+            Console.WriteLine();
+            
+            // put test objects in array
+            SimpleClass[] mcArray = new SimpleClass[6];
+            mcArray[0] = sc1;
+            mcArray[1] = sc2;
+            mcArray[2] = sc3;
+            mcArray[3] = sc4;
+            mcArray[4] = sc5;
+            mcArray[5] = sc6;
+            
+            // sort array
+            Array.Sort(mcArray);
+            
+            // print sorted array
+            foreach (SimpleClass item in mcArray)
+                Console.WriteLine(item);
+        }
+    }
+}
---
+++ Assignment3/Prob5/SimpleClass.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prob5
+{
+    // Create a class with two fields, one a string and the other an int. 
+    // The class implements the IComparable interface.
+    // https://docs.microsoft.com/en-us/dotnet/api/system.icomparable?view=netframework-4.8
+    class SimpleClass : IComparable
+    {
+        // fields
+        private string scStr;
+        private int scInt;
+        
+        // properties to access private members
+        public string ScStr
+        {
+            get { return scStr; }
+            set { scStr = value; }
+        }
+        
+        public int ScInt
+        {
+            get { return scInt; }
+            set { scInt = value; }
+        }
+        
+        // constructor
+        public SimpleClass(string str, int x)
+        {
+            this.scInt = x;
+            this.scStr = str;
+        }
+        
+        // interface method
+        // Objects in the class should be ordered in the following manner:
+        //    * primary sort order is alphabetical according to the text field
+        //    * secondary sort order is largest first according to the int field
+        // int IComparable.CompareTo(object obj)
+        public int CompareTo(object obj)
+        {
+            // cast obj to MyClass type
+            SimpleClass sc = obj as SimpleClass;
+            
+            // if cast successful make comparisons
+            if (sc != null)
+            {
+                // return value corresponding to comparison
+                if (String.Compare(this.ScStr, sc.ScStr) < 0) return -1;
+                if (String.Compare(this.ScStr, sc.ScStr) > 0) return 1;
+                else // strings are the same - compare ints
+                {
+                    if (this.ScInt < sc.ScInt) return -1;
+                    if (this.ScInt > sc.ScInt) return 1;
+                    else return 0;
+                }
+            }
+            else
+                throw new ArgumentException("Parameter is not a MyClass object ");
+        }
+        
+        // override ToString so can print object
+        public override string ToString()
+        {
+            return $"{ScStr}, {ScInt}";
+        }
+    }
+}
